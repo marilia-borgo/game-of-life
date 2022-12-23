f4b7d2f32b87a52b9e28cbe809535b762b53fa22
marilia-borgo <marilia.bm01@gmail.com>
2022-12-23 17:41:53 -0300

    alterações vue

--- life_vue/src/components/HelloWorld.vue
+++ life_vue/src/components/HelloWorld.vue
@@ -1,52 +1,93 @@
 <template>
   <table>
-    <td v-for="(line, r) in grid" :key="JSON.stringify([line, r])">
-      <tr v-for="(coluna, c) in line" :key="JSON.stringify([r, c, coluna])">
-        <p :style="pintaQuadrado(coluna)">&nbsp;</p>
-      </tr>
-    </td>
+    <tr v-for="(line, r) in grid" :key="`${line}-${r}`">
+      <td
+        v-for="(coluna, c) in line"
+        :key="`${r}-${c}-${coluna}`"
+        :class="classes(r, c)"
+      ></td>
+    </tr>
   </table>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        grid: [
-          [0,1,0,0],
-          [0,0,1,0],
-          [0,0,0,1],
-          [1,0,0,0]
-        ],
-        count:0,
-      }
-    },
-    methods:{
-      update(){
-        for(var i = 0; i < this.grid.length; i++) {
-            var cube = this.grid[i];
-            for(var j = 0; j < cube.length; j++) {
-                this.grid[i][j] = Math.random() > 0.5 ? 1 : 0
+export default {
+  data() {
+    return {
+      grid: [
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0],
+      ],
+      lines: 4,
+      columns: 5,
+    };
+  },
+  methods: {
+    update() {
+      let new_matriz = JSON.parse(JSON.stringify(this.grid))
+      for (let i = 0; i < this.lines; i++) {
+        for (let j = 0; j < this.columns; j++) {
+          let alives = this.vizinhos(i, j);
+          if (this.grid[i][j] === 1) {
+            if (alives < 2 || alives > 3) {
+              new_matriz[i][j] = 0
             }
+          } else {
+            if (alives === 3) {
+              new_matriz[i][j] = 1
+            }
+          }
+
         }
-        this.grid.push([])
-        this.grid.pop()
-      },
-      pintaQuadrado(coluna){
-        if(coluna === 1){
-          return 'background-color:black'
-        }
-        return 'background-color:white'
       }
+      this.grid = new_matriz;
+      return new_matriz
     },
-    created(){
-      const acabate = ()=> {
-        this.update()
-        this.count++
-        console.log("aaaa")
-        setTimeout(acabate, 500)
+    vizinhos(x, y) {
+      let somatorio = 0;
+      for (let linha_x of [-1, 0, 1]) {
+        for (let coluna_y of [-1, 0, 1]) {
+          const vizinho_x = x + linha_x;
+          const vizinho_y = y + coluna_y;
+          if (
+            vizinho_x < 0 ||
+            vizinho_y < 0 ||
+            vizinho_x >= this.grid.length ||
+            vizinho_y >= this.grid[0].length) continue
+          if (vizinho_x == x && vizinho_y == y) continue
+          somatorio += this.grid[vizinho_x][vizinho_y]
+        }
       }
-      acabate()
-    }
-  }
+      console.log(`${x} ${y} esse é o somatório ${somatorio}`);
+      return somatorio;
+    },
+    classes(linha, coluna) {
+      console.log(this.grid[linha][coluna]);
+      return {
+        pintado: this.grid[linha][coluna],
+      };
+    },
+  },
+  created() {
+    const acabate = () => {
+      this.update();
+      setTimeout(acabate, 800);
+    };
+    acabate();
+  },
+};
 </script>
+
+<style scoped>
+table td {
+  border: 1px solid #eee;
+  width: 30px;
+  height: 30px;
+  background-color: white;
+}
+.pintado {
+  background-color: black;
+}
+</style>
